2cef06d5e43536e8b82f7a5b135324aded1864bd
stambekovbera <87119093+stambekovbera@users.noreply.github.com>
2022-05-08 20:30:32 +0500

    fix

--- src/layouts/Default/Layout.tsx
+++ src/layouts/Default/Layout.tsx
@@ -1,4 +1,7 @@
 import React, {FC} from 'react';
+import {Box} from "@mui/material";
+import {createUseStyles} from "react-jss";
+import clsx from "clsx";
 
 interface LayoutProps {
     hasFooter: boolean;
@@ -8,11 +11,21 @@ const Layout: FC<LayoutProps> = (props) => {
     const {
         hasFooter
     } = props;
+    const classes = useStyles();
     return (
-        <>
-
-        </>
+        <Box className={clsx({
+            [classes.wrapper]: true,
+        })}>
+        </Box>
     );
 };
+
+const useStyles = createUseStyles({
+    wrapper: {
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "100%",
+    }
+})
 
 export default Layout;
