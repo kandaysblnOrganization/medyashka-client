38e387f4a256b173ad8399e70c8a1d24c6feeaac
stambekovbera <87119093+stambekovbera@users.noreply.github.com>
2022-05-08 22:16:57 +0500

    fix

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -11,25 +11,21 @@ const RouteComponent: FC = () => {
     return (
         <Routes>
             <Route path="/" element={<Layout isAuth={isAuth}/>}>
-                {isAuth && (
-                    <>
-                        {authRoutes.map(route => (
-                            <Route key={route.id} path={route.path} element={route.element}/>
-                        ))}
-                    </>
-                )}
-                {publicRoutes.map(route => (
-                    <>
-                        {route.index
-                            ? (
-                                <Route key={route.id} index element={route.element}/>
-                            )
-                            : (
-                                <Route key={route.id} path={route.path} element={route.element}/>
-                            )
-                        }
-                    </>
-                ))}
+                <>
+                    {isAuth && (
+                        <>
+                            {authRoutes.map(route => <Route key={route.id} path={route.path} element={route.element}/>)}
+                        </>
+                    )}
+                    {publicRoutes.map(route => (
+                        <>
+                            {route.index
+                                ? <Route key={route.id} index element={route.element}/>
+                                : <Route key={route.id} path={route.path} element={route.element}/>
+                            }
+                        </>
+                    ))}
+                </>
             </Route>
         </Routes>
     );
