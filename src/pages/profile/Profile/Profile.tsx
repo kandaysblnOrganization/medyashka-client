5d0bc824267b609bdd7eb621c2011dce1d79c09b
stambekovbera <87119093+stambekovbera@users.noreply.github.com>
2022-05-30 23:01:37 +0500

    fix server and client

--- src/pages/profile/Profile/Profile.tsx
+++ src/pages/profile/Profile/Profile.tsx
@@ -1,10 +1,59 @@
-import React, {FC} from 'react';
+import {Container} from '@mui/material';
+import React, {FC, useEffect, useState} from 'react';
+import {agent} from "../../../api/agent";
+import {IResponseUserImage, IResponseUserProgress} from "../../../types/ResponseTypes";
+import {IError} from "../../../types/ErrorTypes";
+import {useTypedSelector} from "../../../hooks/redux/useTypedSelector";
+import {Notification, NotificationTypes} from "../../../common/Notification";
 
-const Profile: FC = () => {
+interface ProfileProps {
+}
+
+const Profile: FC<ProfileProps> = (props) => {
+    const {user} = useTypedSelector(state => state.user);
+    const [userProgress, setUserProgress] = useState<IResponseUserProgress | {}>({});
+    const [userImage, setUserImage] = useState<IResponseUserImage | {}>({});
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        (async () => {
+            await getUserImage();
+            await getUserProgress();
+        })()
+    }, []);
+
+    const getUserProgress = async () => {
+        const response: IResponseUserProgress | IError = await agent.get(`medya-api/progress`)
+            .then(res => res.data)
+            .catch(err => {
+                return {error: err.response.data.message}
+            });
+
+        await setUserProgress(response);
+        setIsLoading(false);
+
+    }
+
+    const getUserImage = async () => {
+        const response: IResponseUserImage | IError = await agent.get<IResponseUserImage>(`medya-api/user-image`)
+            .then(res => {
+                return {
+                    id: res.data.id,
+                    avatar: res.data.avatar,
+                }
+            })
+            .catch(err => {
+                return {error: err.response.data.message}
+            });
+
+        await setUserImage(response);
+    }
     return (
-        <div>
-            ПРОФИль
-        </div>
+        <>
+            <Container maxWidth="xl">
+
+            </Container>
+        </>
     );
 };
 
--- src/types/ResponseTypes/index.ts
+++ src/types/ResponseTypes/index.ts
@@ -1,3 +1,17 @@
 export interface IResponseUser {
     token: string;
 }
+
+export interface IResponseUserProgress {
+    id: number;
+    percent_progress: number | string;
+    first_book_last_page: number | string;
+    second_book_last_page: number | string;
+    third_book_last_page: number | string;
+    fourth_book_last_page: number | string;
+}
+
+export interface IResponseUserImage {
+    id: number;
+    avatar: string;
+}
