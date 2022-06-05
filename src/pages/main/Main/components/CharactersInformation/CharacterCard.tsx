2915fe224b8f88186244925ee6b6a3fdb01c343f
stambekovbera <87119093+stambekovbera@users.noreply.github.com>
2022-06-05 16:06:26 +0500

    fix characters cards

--- src/pages/main/Main/components/CharactersInformation/CharacterCard.tsx
+++ src/pages/main/Main/components/CharactersInformation/CharacterCard.tsx
@@ -3,9 +3,15 @@ import {ICharacterCard} from "../../../../../types/ContantsTypes";
 import {Box, Grid, Tooltip, Typography} from "@mui/material";
 import {createUseStyles} from "react-jss";
 import {ICardsJss} from "../../../../../types/JssProps";
+import {Classes} from "jss";
 
 interface CharacterCardProps {
     characterCard: ICharacterCard;
+}
+
+interface CharacterTooltipProps {
+    characterCard: ICharacterCard;
+    classes: Classes<any>;
 }
 
 const CharacterCard: FC<CharacterCardProps> = (props) => {
@@ -25,7 +31,15 @@ const CharacterCard: FC<CharacterCardProps> = (props) => {
                     </Box>
                 </Grid>
                 <Grid item>
-                    <Tooltip title={characterCard.description} arrow followCursor>
+                    <Tooltip
+                        classes={{
+                            tooltip: classes.tooltipWrapper,
+                            arrow: classes.tooltipArrow,
+                        }}
+                        title={<CharacterTooltip characterCard={characterCard} classes={classes}/>}
+                        arrow
+                        placement="left"
+                    >
                         <Box className={classes.imageWrapper}>
                             <img className={classes.image} src={characterCard.image} alt=""/>
                         </Box>
@@ -35,6 +49,20 @@ const CharacterCard: FC<CharacterCardProps> = (props) => {
         </Box>
     );
 };
+
+const CharacterTooltip: FC<CharacterTooltipProps> = (props) => {
+    const {
+        characterCard,
+        classes
+    } = props;
+    return (
+        <Box>
+            <Typography className={classes.tooltipName} variant='h6'>{characterCard.name}</Typography>
+            <Typography className={classes.tooltipDescription} variant='h6'>{characterCard.description}</Typography>
+        </Box>
+    );
+};
+
 
 const useStyles = createUseStyles({
     nameWrapper: (props: ICardsJss) => ({
@@ -52,14 +80,44 @@ const useStyles = createUseStyles({
         }
     },
     imageWrapper: {
-        width: 350,
-        height: 400,
+        transition: "all .2s linear",
+        "&:hover": {
+            transform: "scale(1.02)",
+        }
     },
     image: {
         width: "100%",
         height: "100%",
-        objectFit: "contain",
-    }
+    },
+
+    tooltipPopper: {},
+    tooltipWrapper: {
+        "&.MuiTooltip-tooltip": {
+            background: "#9282E0",
+            borderRadius: 14,
+            padding: 12,
+        }
+    },
+    tooltipArrow: {
+        "&.MuiTooltip-arrow": {
+            color: "#9282E0",
+        }
+    },
+    tooltipName: (props: ICardsJss) => ({
+        "&.MuiTypography-root": {
+            fontWeight: 700,
+            letterSpacing: "0.045em",
+            color: props.backgroundColor1,
+            marginBottom: 8,
+        }
+    }),
+    tooltipDescription: {
+        "&.MuiTypography-root": {
+            fontWeight: 300,
+            color: "#FFFAF2",
+            letterSpacing: "0.045em",
+        }
+    },
 });
 
 export default CharacterCard;
